8c45e20918204e671dbf55474f6b5e2da2f80ca0
Nicholas Johnson <nejohnson2@users.noreply.github.com>
2015-08-07 12:32:18 -0400

    Create bladerf.rb

---
+++ bladerf.rb
@@ -0,0 +1,19 @@
+require 'formula'
+
+class Bladerf < Formula
+  homepage 'http://nuand.com/'
+  head 'https://github.com/Nuand/bladeRF.git'
+
+  depends_on 'cmake' => :build
+  depends_on 'libusb'
+  depends_on 'libtecla'
+
+  def install
+    cd 'host'
+    mkdir 'build' do
+      system 'cmake', '..', *std_cmake_args << "-DINSTALL_UDEV_RULES=OFF"
+      system 'make'
+      system 'make install'
+    end
+  end
+end
