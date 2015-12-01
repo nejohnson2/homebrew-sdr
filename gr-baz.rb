4831adc5bb43d2a99ddea65ff5728e725e4411c4
Nicholas Johnson <nejohnson2@users.noreply.github.com>
2015-12-01 12:19:22 -0500

    from andresv/homebrew-gnuradio

---
+++ gr-baz.rb
@@ -0,0 +1,17 @@
+require 'formula'
+
+class GrBaz < Formula
+  homepage 'http://wiki.spench.net/wiki/Gr-baz'
+  head 'https://github.com/balint256/gr-baz.git'
+
+  depends_on 'cmake' => :build
+  depends_on 'gnuradio'
+
+  def install
+    mkdir 'build' do
+      system 'cmake', '..', '-DPYTHON_LIBRARY=/usr/local/Frameworks/Python.framework/Versions/2.7/Python ', *std_cmake_args
+      system 'make'
+      system 'make install'
+    end
+  end
+end
