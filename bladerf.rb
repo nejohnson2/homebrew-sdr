cbe6bfadb490983372e9c14650f99124ea34574e
Nicholas Johnson <nejohnson2@users.noreply.github.com>
2015-12-01 12:23:32 -0500

    updated from andresv/homebrew-gnuradio

--- bladerf.rb
+++ bladerf.rb
@@ -1,17 +1,21 @@
 require 'formula'
 
 class Bladerf < Formula
-  homepage 'http://nuand.com/'
+  homepage 'https://github.com/Nuand/bladeRF/wiki'
   head 'https://github.com/Nuand/bladeRF.git'
 
+  depends_on 'pkg-config' => :build
+  depends_on 'automake' => :build
+  depends_on 'libtool' => :build
   depends_on 'cmake' => :build
   depends_on 'libusb'
-  depends_on 'libtecla'
 
   def install
-    cd 'host'
+    cd 'host/utilities/bladeRF-cli/src/cmd/doc' do
+      system 'cp', 'cmd_help.h.in', 'cmd_help.h'
+    end
     mkdir 'build' do
-      system 'cmake', '..', *std_cmake_args << "-DINSTALL_UDEV_RULES=OFF"
+      system 'cmake', '..', *std_cmake_args
       system 'make'
       system 'make install'
     end
