a1e0b17317927c529151f4de997ea9c00dd197fc
Nicholas Johnson <nejohnson2@users.noreply.github.com>
2015-12-01 12:12:04 -0500

    updated from andresv/homebrew-gnuradio

--- gqrx.rb
+++ gqrx.rb
@@ -1,28 +1,22 @@
-require "formula"
+require 'formula'
 
 class Gqrx < Formula
-  homepage "http://gqrx.dk"
-  url "https://github.com/csete/gqrx/archive/v2.3.2.tar.gz"
-  sha1 "23680d9527aca135c2952ab17873ab716e379ac6"
+  homepage 'https://github.com/csete/gqrx'
+  head 'https://github.com/csete/gqrx.git'
 
-  head "https://github.com/csete/gqrx.git"
-
+  depends_on 'cmake' => :build
   depends_on 'pkg-config' => :build
-  depends_on 'gnuradio'
-  depends_on 'rtlsdr'
-  depends_on 'gr-osmosdr'
   depends_on 'qt'
+  depends_on 'boost'
+  depends_on 'gnuradio'
+  depends_on 'gr-osmosdr'
 
   def install
     args = "PREFIX=#{prefix}"
     mkdir "build" do
-      system "qmake", "..", *args
+      system "qmake",  *args, ".."
       system "make"
     end
     Dir.glob("build/*.app") { |app| mv app, prefix }
-  end
-
-  test do
-    system "false"
   end
 end
