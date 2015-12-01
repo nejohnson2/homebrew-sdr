8dd1f7fa1bcbeaedb5bd9de728a30ddf0f6e7336
Nicholas Johnson <nejohnson2@users.noreply.github.com>
2015-12-01 12:13:43 -0500

    Update gr-osmosdr.rb

--- gr-osmosdr.rb
+++ gr-osmosdr.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class GrOsmosdr < Formula
   homepage 'http://sdr.osmocom.org/trac/wiki/GrOsmoSDR'
-  head 'git://git.osmocom.org/gr-osmosdr'
+  head 'git://git.osmocom.org/gr-osmosdr', :shallow => false
 
   depends_on 'cmake' => :build
   depends_on 'gnuradio'
